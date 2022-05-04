e3e1b2215d3b9eaaa9d3004aeb990ac470c7dc06
raulrodriguezlr <12rulo12@gmail.com>
2022-05-04 14:18:08 +0200

    primer commit

---
+++ practicaSSDD2/src/main/java/es/codeurjc/practicaSSDD2/Application.java
@@ -0,0 +1,8 @@
+package es.codeurjc.practicaSSDD2;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ practicaSSDD2/src/test/java/es/codeurjc/practicaSSDD2/ApplicationTest.java
@@ -0,0 +1,11 @@
+package es.codeurjc.practicaSSDD2;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void testMainMethod() {
+
+    }
+}
