06308800c6f7b7eea5874ad2876eede41ee36fe2
raulrodriguezlr <12rulo12@gmail.com>
2022-05-04 17:16:49 +0200

    cambio de proyecto de maven a spring mvc

--- practicaSSDD2/README.md
+++
@@ -1,1 +0,0 @@
-# practicaSSDD2
--- practicaSSDD2/src/main/java/es/codeurjc/practicaSSDD2/Application.java
+++
@@ -1,8 +0,0 @@
-package es.codeurjc.practicaSSDD2;
-
-public class Application {
-
-    public static void main(String[] args) {
-    	System.out.println("REQUENA ES UNA CALVA TOXICA");
-    }
-}
